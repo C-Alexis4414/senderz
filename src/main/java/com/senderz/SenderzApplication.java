b1ff4824be2c35d6e822da80f6016e03182d63d7
alexisc <a.chentreu@protonmail.com>
2023-12-09 22:02:58 +0100

    SenderzApplication locally to remote repository

---
+++ src/main/java/com/senderz/SenderzApplication.java
@@ -0,0 +1,13 @@
+package com.senderz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenderzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenderzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senderz/SenderzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senderz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenderzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
